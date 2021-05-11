2f6782dc0daf2b6fae7444525d07a95a9d302cfb
ricardo ramirez ramirez <ricardoabcd3@gmail.com>
2021-05-10 22:59:22 -0500

    ya se armo

--- debugging.py
+++ debugging.py
@@ -1,6 +1,11 @@
 
 def run():
-    hi=list(map(lambda x: i for range(1,num + 1),my_list))
-print(squares)
+    try:
+        num=int(input("enter number : "))
+    except ValueError:
+        print("wrong value")
+
+    hi = [a for a in range (1, num + 1) if num % a == 1]
+    print(hi)
 if __name__=='__main__':
     run()
