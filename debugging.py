9df35f98ce7ebfb6a1ad6cc5489db86537a26470
ricardo ramirez ramirez <ricardoabcd3@gmail.com>
2021-05-11 20:04:32 -0500

    this works

--- debugging.py
+++ debugging.py
@@ -2,10 +2,23 @@
 def run():
     try:
         num=int(input("enter number : "))
+        hi = [a for a in range (1, num + 1) if num % a == 1]
+        print(hi)
+        a=0
     except ValueError:
         print("wrong value")
+        a=1
+    while a == 1:
+        try:
+            num=int(input("enter number : "))
+            hi = [a for a in range (1, num + 1) if num % a == 1]
+            print(hi)
+            a=0
+        except ValueError:
+            print("wrong value")
+            a=1
 
-    hi = [a for a in range (1, num + 1) if num % a == 1]
-    print(hi)
+
+    
 if __name__=='__main__':
     run()
